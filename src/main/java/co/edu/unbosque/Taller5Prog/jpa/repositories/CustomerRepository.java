d519e06ea9b757e44474c239724d6aa728634894
gitForDavid <dlopezf@unbosque.edu.co>
2021-05-16 14:36:14 -0500

    create listo

--- src/main/java/co/edu/unbosque/Taller5Prog/jpa/repositories/CustomerRepository.java
+++ src/main/java/co/edu/unbosque/Taller5Prog/jpa/repositories/CustomerRepository.java
@@ -1,9 +1,11 @@
 package co.edu.unbosque.Taller5Prog.jpa.repositories;
 
+import co.edu.unbosque.Taller5Prog.jpa.entities.Customer;
+
 public interface CustomerRepository {
 
 
-    void save(String email, String first_name, String last_name, String gender, int age);
+    void save(Customer customer);
 
     void update(String email, String first_name, String last_name, String gender, int age);
 
