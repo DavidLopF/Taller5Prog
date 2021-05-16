2c1440362a78d44e77b186f42ea44847020c26ae
Sergio-1603 <sergioeduardo2000@gmail.com>
2021-05-16 14:59:03 -0500

    requerimiento 1 y 2

--- src/main/webapp/Js/tablaModificarAutor.js
+++ src/main/webapp/Js/tablaModificarAutor.js
@@ -1,21 +1,18 @@
-function printTable() {
+function printTable(){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             var data = JSON.parse(xhr.responseText);
-            var tbodyRef = document.getElementById("res")
-
+            var tbodyRef = document.getElementById("selectAuthor")
             for (let i = 0; i < data.length; i++) {
                 tbodyRef.innerHTML += `
-                <tr>
-                <td>${data[i].libraryId}</td>
-                <td>${data[i].name}</td>
-                </tr>`
+                <option value="${data[i].authorId}">${data[i].name}</option>
+               `
             }
         }
     }
-    xhr.open('GET', 'http://localhost:8080/Taller5Prog-1.0-SNAPSHOT/list-libraries', true);
+    xhr.open('GET', 'http://localhost:8080/JPATutorial-1.0-SNAPSHOT/list-authors', true);
     xhr.send(null)
-}
 
+}
 printTable();
