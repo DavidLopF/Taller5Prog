08a22f6d1a22f879a66403ddae4a05a45538f018
Sergio-1603 <sergioeduardo2000@gmail.com>
2021-05-17 17:14:42 -0500

    Creando primera edicion

--- src/main/java/co/edu/unbosque/Taller5Prog/servlets/CreateBookServlet.java
+++ src/main/java/co/edu/unbosque/Taller5Prog/servlets/CreateBookServlet.java
@@ -1,12 +1,14 @@
 package co.edu.unbosque.Taller5Prog.servlets;
 
 import co.edu.unbosque.Taller5Prog.services.BookService;
+import co.edu.unbosque.Taller5Prog.services.EditionService;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @WebServlet(name = "cretateBookServlet", value = "/create-book")
 public class CreateBookServlet extends HttpServlet {
@@ -20,11 +22,13 @@ public class CreateBookServlet extends HttpServlet {
         String isb = request.getParameter("isbn");
         String genero = request.getParameter("selectLibro");
 
-
-
         BookService bookService = new BookService();
         bookService.saveBook(titulo, isb, autor,genero);
 
+
+        EditionService editionService = new EditionService();
+        Date date = new Date();
+        editionService.saveEdition("Primera edición",date,bookService.findByTitle(titulo).get().getBookId());
         response.sendRedirect("./crearLibro.html");
 
     }
